2e08c9df3b7b75d66c68de0150b2bde168ef3434
Artem Sharov <sharov.a.p.10@gmail.com>
2020-12-14 16:35:41 +0300

    HW_5

---
+++ src/HW_5/BagPackage.java
@@ -0,0 +1,20 @@
+package HW_5;
+
+public class BagPackage {
+    private int value;
+    private int weight;
+
+
+    public BagPackage(int value, int weight) {
+        this.value = value;
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
